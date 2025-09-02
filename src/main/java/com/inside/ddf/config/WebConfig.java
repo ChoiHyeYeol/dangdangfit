b1a5a314d454e9b42de46ae20ddd0f4ecfb65920
hyeyeol <choihyeyeol99@gmail.com>
2025-09-02 15:16:32 +0900

    123

--- src/main/java/com/inside/ddf/config/WebConfig.java
+++ src/main/java/com/inside/ddf/config/WebConfig.java
@@ -1,5 +1,3 @@
-// src/main/java/com/inside/ddf/config/WebConfig.java
-
 package com.inside.ddf.config;
 
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +9,16 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/index.html");
-        registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
-        registry.addViewController("/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}").setViewName("forward:/index.html");
+        // 기본 진입점
+        registry.addViewController("/")
+                .setViewName("forward:/index.html");
+
+        // /my, /recipe 같은 단일 뎁스 라우팅
+        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}")
+                .setViewName("forward:/index.html");
+
+        // 다중 뎁스 라우팅(/my/edit/info 같은 경우)
+        registry.addViewController("/**/{spring:[a-zA-Z0-9-_]+}")
+                .setViewName("forward:/index.html");
     }
 }
