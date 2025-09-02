8f26a3d306346754792b542d4e1bc72ea0d5c0a2
hyeyeol <choihyeyeol99@gmail.com>
2025-09-02 14:27:55 +0900

    2-27

--- src/main/java/com/inside/ddf/controller/MainController.java
+++ src/main/java/com/inside/ddf/controller/MainController.java
@@ -2,7 +2,6 @@ package com.inside.ddf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MainController {
@@ -15,9 +14,4 @@ public class MainController {
 //	public String reindex() {
 //		return "redirect:/index";
 //	}
-	// 정적 파일이 아닌 모든 경로는 index.html로 전달
-    @GetMapping("/{path:^(?!api|static|css|js|images|fonts|favicon\\.ico).*$}")
-    public String redirect(@PathVariable String path) {
-        return "forward:/index.html";
-    }
 }
